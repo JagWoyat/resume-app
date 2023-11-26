06df0b9934fb2e90e193b609ddf2c7b432a08654
Ja <ja@ja.lan>
2023-11-26 23:23:18 +0100

    improved routes

--- src/routes/CryptoView.jsx
+++ src/routes/CryptoView.jsx
@@ -1,11 +1,30 @@
 import React from "react";
-import Icon from "../components/common/Icon";
 import AppOverview from "../components/AppOverview/AppOverview";
+import bg from "../assets/maxim-hopman-fiXLQXAhCfk-unsplash.jpg";
+import Background from "../components/common/Background";
+
+const data = {
+  name: "Cryptocurrency App",
+  description: (
+    <p>
+      Application utilizing{" "}
+      <a href="https://developers.coinranking.com/api">Coinranking API</a> to
+      display information about user's chosen cryptocurrencies. You can explore
+      list of the biggest cryptocurrencies by market cap and check individual
+      profiles of them, where you can add them to the list of favorites. App
+      also supports adding custom cryptocurrencies to the list of favorites. All
+      saved cryptocurrencies all saved in the browser.
+    </p>
+  ),
+  tools: ["React", "TypeScript", "React Router", "Styled Components", "Redux"],
+  link: "https://elegant-dasik-a33ddd.netlify.app/",
+};
 
 export default function CryptoView() {
   return (
     <main>
-      <AppOverview />
+      <Background background={bg} />
+      <AppOverview data={data} />
     </main>
   );
 }
--- src/routes/WeatherView.jsx
+++ src/routes/WeatherView.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import AppOverview from "../components/AppOverview/AppOverview";
+import bg from "../assets/wolfgang-hasselmann-bR_-gllg7Bs-unsplash.jpg";
+import Background from "../components/common/Background";
+
+const data = {
+  name: "Weather App",
+  description: (
+    <p>
+      Application utilizing{" "}
+      <a href="https://openweathermap.org/api">OpenWeather</a> to display
+      information about user's chosen city's weather. You can explore list of
+      the biggest cryptocurrencies by market cap and check individual profiles
+      of them, where you can add them to the list of favorites. App also
+      supports adding custom cryptocurrencies to the list of favorites. All
+      saved cryptocurrencies all saved in the browser.
+    </p>
+  ),
+  tools: ["React", "React Router", "Redux"],
+  link: "https://elegant-dasik-a33ddd.netlify.app/",
+};
 
 export default function WeatherView() {
-  return <div>WeatherView</div>;
+  return (
+    <main>
+      <Background background={bg} />
+      <AppOverview data={data} />
+    </main>
+  );
 }
