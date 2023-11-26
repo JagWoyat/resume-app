9ca8ff907e1d2735a6710657f8e4b1f37578db33
Ja <ja@ja.lan>
2023-11-26 23:23:34 +0100

    styling improvments

--- src/components/Sidebar/Title.jsx
+++ src/components/Sidebar/Title.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Title.module.css";
 import { Link } from "react-router-dom";
-import icon from "./../../asstets/add.svg";
+import icon from "./../../assets/add.svg";
 import Icon from "../common/Icon";
 
 export default function Title() {
