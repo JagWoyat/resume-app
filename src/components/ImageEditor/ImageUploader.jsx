d69021c1744053d01428dea8a8bdff6a8873a411
Ja <ja@ja.lan>
2024-01-09 22:41:26 +0100

    added dynamic edition modifiers

--- src/components/ImageEditor/ImageUploader.jsx
+++ src/components/ImageEditor/ImageUploader.jsx
@@ -1,15 +1,71 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styles from "./ImageUploader.module.css";
 
 const API_URL = "http://98.71.35.179:4000/image";
 
 export default function ImageUploader({ title, request }) {
   const [image, setImage] = useState();
+  const [imagePreview, setImagePreview] = useState(null);
+  const [dragActive, setDragActive] = useState(false);
+  const inputRef = useRef();
+
+  const mirHorRef = useRef();
+  const mirVerRef = useRef();
+  const grayscaleRef = useRef();
+  const heightRef = useRef();
+  const widthRef = useRef();
+
+  const [resize, setResize] = useState(false);
 
   let navigate = useNavigate();
 
   const onFileChange = (event) => {
-    setImage(event.target.files[0]);
+    const file = event.target.files[0];
+
+    if (file) {
+      setImage(file);
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        setImagePreview(e.target.result);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const removeImg = () => {
+    setImage(undefined);
+    setImagePreview(null);
+  };
+
+  const handleDrag = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (event.type === "dragenter" || event.type === "dragover") {
+      setDragActive(true);
+    } else if (event.type === "dragleave") {
+      setDragActive(false);
+    }
+  };
+
+  const handleDrop = function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    setDragActive(false);
+
+    const file = event.dataTransfer?.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        setImagePreview(event.target?.result);
+      };
+
+      reader.readAsDataURL(file);
+    }
   };
 
   const handleSubmit = (event) => {
@@ -17,14 +73,20 @@ export default function ImageUploader({ title, request }) {
 
     let formData = new FormData();
 
+    const mirHor = mirHorRef.current.checked;
+    const mirVer = mirVerRef.current.checked;
+    const grayscale = grayscaleRef.current.checked;
+    const height = parseInt(heightRef.current.value, 10);
+    const width = parseInt(widthRef.current.value, 10);
+
     if (!image) return;
     formData.append("image", image);
     formData.append("name", "random-name");
-    formData.append("mirrored_h", false);
-    formData.append("mirrored_v", true);
-    formData.append("height", 0);
-    formData.append("width", 900);
-    formData.append("grayscale", false);
+    formData.append("mirrored_h", mirHor);
+    formData.append("mirrored_v", mirVer);
+    formData.append("height", height);
+    formData.append("width", width);
+    formData.append("grayscale", grayscale);
 
     // console.log(formData);
     const url = API_URL;
@@ -45,13 +107,122 @@ export default function ImageUploader({ title, request }) {
       });
   };
 
+  const handleClick = () => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    }
+  };
+
   return (
-    <div>
+    <div className={styles.UploaderWrapper}>
       <h1>{title}</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="file" onChange={onFileChange} />
-        <button type="submit">Upload</button>
-      </form>
+      {imagePreview ? (
+        <form
+          className={styles.Form}
+          onDragEnter={handleDrag}
+          onSubmit={handleSubmit}
+        >
+          <button className={styles.RemoveImgButton} onClick={removeImg}>
+            X
+          </button>
+          <img
+            src={imagePreview}
+            alt="SelectedImage"
+            style={{ maxWidth: "100%" }}
+          />
+          <fieldset className={styles.Fieldset}>
+            <legend>Modifiers</legend>
+            <label>
+              <input type="checkbox" ref={mirHorRef} />
+              Mirrored horizontally
+            </label>
+            <label>
+              <input type="checkbox" ref={mirVerRef} />
+              Mirrored vertically
+            </label>
+            <label>
+              <input type="checkbox" ref={grayscaleRef} />
+              Turn to Grayscale
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                checked={resize}
+                onChange={(e) => setResize(e.target.checked)}
+              />
+              Resize image
+            </label>
+            {resize && (
+              <>
+                <p>Leave value at 0 to keep aspect ratio</p>
+                <label>
+                  Width:
+                  <input type="number" defaultValue="0" ref={widthRef} />
+                </label>
+                <label>
+                  Height:
+                  <input type="number" defaultValue="0" ref={heightRef} />
+                </label>
+              </>
+            )}
+          </fieldset>
+          <button
+            className={styles.UploadButton}
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Upload a file
+          </button>
+
+          <input
+            ref={inputRef}
+            style={{ display: "none" }}
+            type="file"
+            id="image-input"
+          />
+        </form>
+      ) : (
+        <form
+          className={styles.Form}
+          onDragEnter={handleDrag}
+          onSubmit={handleSubmit}
+        >
+          <label
+            className={dragActive ? styles.LabelActive : styles.Label}
+            htmlFor="image-input"
+          >
+            <input
+              ref={inputRef}
+              style={{ display: "none" }}
+              type="file"
+              id="image-input"
+              onChange={onFileChange}
+            />
+            <div>
+              <p>Drag and drop your file here or</p>
+              <button onClick={handleClick} className={styles.UploadButton}>
+                Upload a file
+              </button>
+            </div>
+          </label>
+          {dragActive && (
+            <div
+              className={styles.ActiveDropZone}
+              onDragEnter={handleDrag}
+              onDragLeave={handleDrag}
+              onDragOver={handleDrag}
+              onDrop={handleDrop}
+            ></div>
+          )}
+        </form>
+      )}
     </div>
+    // <div>
+    //   <h1>{title}</h1>
+    //   <form onSubmit={handleSubmit}>
+    //     <input type="file" onChange={onFileChange} />
+    //     <button type="submit">Upload</button>
+    //   </form>
+    // </div>
   );
 }
