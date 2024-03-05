9b95222227a195fc49168219bc9dafd08d3f4a45
JagWoyat <jagwoyat@tutanota.com>
2024-03-05 14:50:18 +0100

    added optional links and titles for carousel component

--- src/components/AppOverview/Carousel.jsx
+++ src/components/AppOverview/Carousel.jsx
@@ -3,55 +3,70 @@ import styles from "./Carousel.module.css";
 import Image from "../InfiniteScroll/Image";
 
 export default function Carousel({ items }) {
-  const [activeIndex, setActiveIndex] = useState(0);
-  function updateIndex(newIndex) {
-    if (newIndex < 0) {
-      newIndex = items.length - 1;
-    } else if (newIndex >= items.length) {
-      newIndex = 0;
-    }
-    setActiveIndex(newIndex);
-  }
+	const [activeIndex, setActiveIndex] = useState(0);
+	function updateIndex(newIndex) {
+		if (newIndex < 0) {
+			newIndex = items.length - 1;
+		} else if (newIndex >= items.length) {
+			newIndex = 0;
+		}
+		setActiveIndex(newIndex);
+	}
 
-  return (
-    <section className={styles.carousel}>
-      <button
-        className={`${styles.button} ${styles.left}`}
-        onClick={() => {
-          updateIndex(activeIndex - 1);
-        }}
-      >
-        <span>&lt;</span>
-      </button>
-      <div
-        className={styles.inner}
-        style={{ transform: `translate(-${activeIndex * 100}%)` }}
-      >
-        {items.map((item) => {
-          return (
-            <div
-              key={item.id}
-              className={styles.carousel_item}
-              style={{ width: "100%" }}
-            >
-              <Image
-                className={styles.carousel_img}
-                src={item.icon}
-                width="600px"
-                height="400px"
-              />
-            </div>
-          );
-        })}
-      </div>
-      <button
-        className={`${styles.button} ${styles.right}`}
-        onClick={() => {
-          updateIndex(activeIndex + 1);
-        }}
-      >
-        <span>&gt;</span>
-      </button>
-    </section>
-  );
+	return (
+		<section className={styles.carousel}>
+			<button
+				className={`${styles.button} ${styles.left}`}
+				onClick={() => {
+					updateIndex(activeIndex - 1);
+				}}
+			>
+				<span>&lt;</span>
+			</button>
+			<div
+				className={styles.inner}
+				style={{ transform: `translate(-${activeIndex * 100}%)` }}
+			>
+				{items.map((item) => {
+					let content = null;
+
+					if (item.title) {
+						if (item.link) {
+							content = (
+								<a target="_blank" href={item.link}>
+									<strong>{item.title}</strong>
+								</a>
+							);
+						} else {
+							content = <strong>{item.title}</strong>;
+						}
+					}
+
+					return (
+						<div
+							key={item.id}
+							className={styles.carousel_item}
+							style={{ width: "100%" }}
+						>
+							<Image
+								className={styles.carousel_img}
+								src={item.icon}
+								width="600px"
+								height="400px"
+							/>
+							{content && <div className={styles.card_title}>{content}</div>}
+						</div>
+					);
+				})}
+			</div>
+			<button
+				className={`${styles.button} ${styles.right}`}
+				onClick={() => {
+					updateIndex(activeIndex + 1);
+				}}
+			>
+				<span>&gt;</span>
+			</button>
+		</section>
+	);
 }
--- src/components/common/Background.jsx
+++ src/components/common/Background.jsx
@@ -2,29 +2,29 @@ import { useState } from "react";
 import styles from "./Background.module.css";
 
 export default function Background({ background, placeholder }) {
-  const [placeholderLoaded, setPlaceholderLoaded] = useState(false);
+	const [placeholderLoaded, setPlaceholderLoaded] = useState(false);
 
-  function handleLoaded() {
-    setTimeout(() => setPlaceholderLoaded(true), 1000);
-  }
+	function handleLoaded() {
+		setTimeout(() => setPlaceholderLoaded(true), 1000);
+	}
 
-  return (
-    <section>
-      <div className={styles.Background}>
-        {!placeholderLoaded && placeholder && (
-          <img src={placeholder} alt="Placeholder" width="100%" height="70%" />
-        )}
-        <img
-          src={background}
-          alt="Background Image"
-          width="100%"
-          height="70%"
-          onLoad={handleLoaded}
-          style={
-            !placeholderLoaded ? { display: "none" } : { display: "block" }
-          }
-        />
-      </div>
-    </section>
-  );
+	return (
+		<section>
+			<div className={styles.Background}>
+				{!placeholderLoaded && placeholder && (
+					<img src={placeholder} alt="Placeholder" width="100%" height="70%" />
+				)}
+				<img
+					src={background}
+					alt="Background Image"
+					width="100%"
+					height="70%"
+					onLoad={handleLoaded}
+					style={
+						!placeholderLoaded ? { display: "none" } : { display: "block" }
+					}
+				/>
+			</div>
+		</section>
+	);
 }
